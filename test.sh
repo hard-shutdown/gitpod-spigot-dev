d3b3f44ce279bbd030338aa98587f34fa0399189
Cody Bohannan <59001842+hard-shutdown@users.noreply.github.com>
2022-02-23 15:47:16 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
-#aritfact name goes here
-
-cp target/demoplugin-1.0-SNAPSHOT.jar server/plugins/demoplugin-1.0-SNAPSHOT.jar
+cp plugin/target/demoplugin-1.0-SNAPSHOT.jar server/plugins/demoplugin-1.0-SNAPSHOT.jar
 cd server
 java -jar server.jar
